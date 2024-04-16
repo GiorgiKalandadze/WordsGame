9c4e7bec003f0df20158f28b3d6e45a8864ef8cd
Giorgi Kalandadze <giorgikalandadze24@gmail.com>
2024-04-16 22:29:14 +0400

    add files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import {WordView} from './modules/words/WordView';
+
+function App() {
+  return (
+    <div className="App">
+      <WordView></WordView>
+    </div>
+  );
+}
+
+export default App;
